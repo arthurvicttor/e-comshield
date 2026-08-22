0aa3c6dd4b78322a4ca472410083fb073682a14c
Arthur Victor <140616974+arthurvicttor@users.noreply.github.com>
2026-08-22 19:33:19 -0300

    feat: enhance authentication by adding dependency injection for form data in login route

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.security.jwt import create_access_token
@@ -6,10 +6,38 @@ from app.security.password import verify_password
 
 router = APIRouter(
     prefix="/auth",
-    tags=["Authentication"]
+    tags=["Authentication"],
 )
 
 fake_user = {
     "username": "admin",
-    "hashed_password": "$2b$12$example"
+    "hashed_password": "$2b$12$tO21q5wD3YQCNuY/Xbm/XucRN47IebTaEfesjMFkpeOUFrtvAaPce",
 }
+
+@router.post("/token")
+async def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+):
+    if form_data.username != fake_user["username"]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+
+    if not verify_password(
+        form_data.password,
+        fake_user["hashed_password"],
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+
+    access_token = create_access_token(
+        data={"sub": fake_user["username"]}
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+     }
